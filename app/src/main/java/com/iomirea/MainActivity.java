3a449072fe3d3efc4e68ae3b9602c489c430d103
novikovikbo <31614731+novikovikbo@users.noreply.github.com>
2019-04-10 01:06:05 +0300

    Added localization

--- app/src/main/java/com/iomirea/MainActivity.java
+++ app/src/main/java/com/iomirea/MainActivity.java
@@ -4,12 +4,15 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Intent intent = new Intent(getApplicationContext(), ChatActivity.class);
         startActivity(intent);
         finish();
