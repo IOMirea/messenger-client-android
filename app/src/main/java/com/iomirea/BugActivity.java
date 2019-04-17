f4eb925e3a3450b30f3b8dc918140030f27b31b6
Eugene <gish.ee18@gmail.com>
2019-04-17 16:57:18 +0300

    Отправка багрепорта с помощью json вместо form_data

--- app/src/main/java/com/iomirea/BugActivity.java
+++ app/src/main/java/com/iomirea/BugActivity.java
@@ -3,9 +3,9 @@ package com.iomirea;
 import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.CheckBox;
@@ -16,8 +16,10 @@ import android.widget.Toast;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.iomirea.http.VolleyController;
+
+import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -36,17 +38,38 @@ public class BugActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
-        ImageButton bug_send = findViewById(R.id.bug_send);
-        bug_send.setOnClickListener(new View.OnClickListener() {
+        ImageButton sendButton = findViewById(R.id.bug_send);
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 final Context context = getApplicationContext();
-                if(bugEditText.getText().toString().trim().length() >= 5){
-                StringRequest reportRequest = new StringRequest(Request.Method.POST,
-                        "https://iomirea.ml/api/v0/bugreports",
-                        new Response.Listener<String>() {
+
+                String bugBody = bugEditText.getText().toString().trim();
+
+                if (bugBody.length() < 5) {
+                    Toast.makeText(
+                            context,
+                            getResources().getString(
+                                    R.string.bugreport_mintext
+                            ),
+                            Toast.LENGTH_SHORT
+                    ).show();
+
+                    return;
+                }
+
+                Map<String, String> data = new HashMap<>();
+
+                data.put("body", bugBody);
+                data.put("device_info", bug_checkbox.isChecked() ? getDeviceInfo() : "");
+                data.put("automatic", "0");
+
+                JsonObjectRequest reportRequest = new JsonObjectRequest(Request.Method.POST,
+                        "https://iomirea.ml/api/bugreports", new JSONObject(data),
+                        new Response.Listener<JSONObject>() {
                             @Override
-                            public void onResponse(String response) {
+                            public void onResponse(JSONObject response) {
                                 Toast.makeText(
                                         context,
                                         getResources().getString(
@@ -70,41 +93,16 @@ public class BugActivity extends AppCompatActivity {
                             ).show();
                         }
                     }
-                }) {
-                    @Override
-                    public String getBodyContentType() {
-                        return "application/x-www-form-urlencoded; charset=UTF-8";
-                    }
-
-                    @Override
-                    protected Map<String, String> getParams() {
-                        Map<String, String> params = new HashMap<>();
-                        params.put("body", bugEditText.getText().toString());
-                        params.put("device_info", bug_checkbox.isChecked() ? getDeviceInfo() : "");
-                        params.put("automatic", "0");
-
-                        return params;
-                    }
-                };
+                });
 
                 VolleyController.getInstance(getApplicationContext()).addToRequestQueue(reportRequest);
-            }
-            else {
-                    Toast.makeText(
-                            context,
-                            getResources().getString(
-                                    R.string.bugreport_mintext
-                            ),
-                            Toast.LENGTH_SHORT
-                    ).show();
-                }
             }
         });
 
     }
 
     private String getDeviceInfo() {
-        String version = "unfound";
+        String version = "Unknown";
         try {
             PackageInfo pInfo = this.getPackageManager().getPackageInfo(getPackageName(), 0);
             version = pInfo.versionName;
@@ -112,7 +110,7 @@ public class BugActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
-        return "app version: " + version +
+        return "App version: " + version +
                 "\n OS version: " + android.os.Build.VERSION.RELEASE +
                 "\n API version: " + android.os.Build.VERSION.SDK_INT +
                 "\n Device name: " + android.os.Build.DEVICE +
@@ -121,13 +119,13 @@ public class BugActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onSupportNavigateUp(){
+    public boolean onSupportNavigateUp() {
         onBackPressed();
         return true;
     }
 
     @Override
-    protected void onRestoreInstanceState(Bundle savedInstanceState){
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
 
         String stateSaved = savedInstanceState.getString("bug_state");
@@ -136,8 +134,7 @@ public class BugActivity extends AppCompatActivity {
             Toast.makeText(BugActivity.this,
                     "onRestoreInstanceState: \nNO state saved!",
                     Toast.LENGTH_LONG).show();
-        }
-        else {
+        } else {
             Toast.makeText(BugActivity.this,
                     "onRestoreInstanceState: \nstate saved!",
                     Toast.LENGTH_LONG).show();
@@ -146,7 +143,7 @@ public class BugActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onSaveInstanceState(Bundle outState){
+    protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
 
         String stateToSave = bugEditText.getText().toString();
@@ -169,7 +166,7 @@ public class BugActivity extends AppCompatActivity {
 
             @Override
             public void run() {
-                doubleBackToExitPressedOnce=false;
+                doubleBackToExitPressedOnce = false;
             }
         }, 2000);
     }
